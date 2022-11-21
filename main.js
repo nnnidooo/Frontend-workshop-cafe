017f1365681da88494d1043fdfd0ed9fcfcefcf5
nnnidooo <nadi3252@stud.kea.dk>
2022-11-21 11:07:20 +0100

    styling for the <p> tags

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 
-
 fetch('http://localhost:3000/cafes')
     .then(response => response.json())
     .then(function (cafes) {
@@ -17,11 +16,10 @@ function renderCafes(cafes) {
         const li = document.createElement('li');
         li.innerHTML = `
             <p class="name">${cafe.cafe_name}</p>
-            <p class="price">${cafe.price_range}</p>
+            <p class="price"> Price range:  ${cafe.price_range}</p>
             <p class="location">${cafe.location}</p>
-            <p class="wifi">${cafe.wifi}</p>
-            <p class="ratings">${cafe.ratings}</p>
-
+            <p class="wifi">Is there wifi at the cafe?  ${cafe.wifi}</p>
+            <p class="ratings">Ratings out of 5 stars ⭐ ️${cafe.ratings}</p>
         `;
         ul.appendChild(li);
     }
